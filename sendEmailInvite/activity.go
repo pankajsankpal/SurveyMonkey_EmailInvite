722808f4fd08f0d199adac5e7df17f9b717a02a4
pankaj <sankpal22pankaj@gmail.com>
2018-05-08 15:00:16 +0530

    added error object as a return type

--- sendEmailInvite/activity.go
+++ sendEmailInvite/activity.go
@@ -2,6 +2,7 @@ package sendEmailInvite
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -31,7 +32,7 @@ func NewActivity(metadata *activity.Metadata) activity.Activity {
 func (a *MyActivity) Metadata() *activity.Metadata {
 	return a.metadata
 }
-func callUrl(method string, url string, bodyContent *bytes.Buffer, accessToken string) string {
+func callUrl(method string, url string, bodyContent *bytes.Buffer, accessToken string) (succ string, err error) {
 
 	request, _ := http.NewRequest(method, url, bodyContent)
 	request.Header.Set("Authorization", "bearer "+accessToken)
@@ -39,19 +40,16 @@ func callUrl(method string, url string, bodyContent *bytes.Buffer, accessToken s
 	client := &http.Client{}
 	succ_resp, error_resp := client.Do(request)
 	if error_resp != nil {
-		outResult := `{ "Error" : { "message" : "The HTTP request for getting SurveyID failed with error ` + error_resp.Error() + `" } }`
-		//context.SetOutput("status", outResult)
-		return outResult
+		return "", error_resp
 	} else {
 		survey_response, _ := ioutil.ReadAll(succ_resp.Body)
 		hasError := gjson.Get(string(survey_response), "error.http_status_code").String()
 		if hasError != "" {
 			outResult := `{ "Error" : { "message" : ` + gjson.Get(string(survey_response), "error.message").String() + ` } }`
 			fmt.Println(outResult)
-			//context.SetOutput("status", outResult)
-			return outResult
+			return "", errors.New(outResult)
 		} else {
-			return string(survey_response)
+			return string(survey_response), nil
 		}
 	}
 }
@@ -69,26 +67,25 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	body := context.GetInput("Body").(string)
 	surveyID := ""
 	method := ""
-	containError := ""
+	//containError := ""
 	isInvite := false
 	if typeofEmail == "invite" {
 		isInvite = true
 	}
 
 	//get surveyId : API Call #1
+
 	method = "GET"
 	log.Debugf("The Incoming Survey Name [%s]", surveyName)
 	surveyIdurl := "https://api.surveymonkey.com/v3/surveys?title=" + surveyName
 	var jsonBody = []byte("")
-	reqSurveyID := callUrl(method, surveyIdurl, bytes.NewBuffer(jsonBody), accessToken)
-	//fmt.Println(reqSurveyID)
-	containError = gjson.Get(string(reqSurveyID), "Error").String()
-	if containError != "" {
-		context.SetOutput("status", containError)
-		return true, nil
+	reqSurveyID, err := callUrl(method, surveyIdurl, bytes.NewBuffer(jsonBody), accessToken)
+	if err != nil {
+		log.Debugf("Error while fetching surveyID [%s]", err.Error())
+		return false, err
 	} else {
 		surveyID = gjson.Get(reqSurveyID, "data.0.id").String()
-		log.Debugf("srveyId: [%s]" surveyID)
+		log.Debugf("srveyId: [%s]", surveyID)
 		fmt.Println("surveyId: " + surveyID)
 	}
 
@@ -98,11 +95,10 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	reqCollectorID := ""
 	method = "GET"
 	jsonBody = []byte("")
-	reqCollectorID = callUrl(method, collector_url, bytes.NewBuffer(jsonBody), accessToken)
-	containError = gjson.Get(string(reqCollectorID), "Error").String()
-	if containError != "" {
-		context.SetOutput("status", containError)
-		return true, nil
+	reqCollectorID, err = callUrl(method, collector_url, bytes.NewBuffer(jsonBody), accessToken)
+	if err != nil {
+		log.Debugf("Error while fetching collectorID: [%s]", err.Error())
+		return false, err
 	} else {
 		collectorList := gjson.Get(reqCollectorID, "data")
 		for _, item := range collectorList.Array() {
@@ -114,13 +110,12 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 		}
 	}
 	if collectorID == "" {
-		//collector already available so reuse it
 		method = "POST"
 		jsonBody = []byte(`{"type":"email","sender_email":"` + senderEmail + `"}`)
-		reqCollectorID = callUrl(method, collector_url, bytes.NewBuffer(jsonBody), accessToken)
-		if containError != "" {
-			context.SetOutput("status", containError)
-			return true, nil
+		reqCollectorID, err = callUrl(method, collector_url, bytes.NewBuffer(jsonBody), accessToken)
+		if err != nil {
+			log.Debugf("Error while fetching collectorID [%s] ", err.Error())
+			return false, err
 		} else {
 			collectorID = gjson.Get(reqCollectorID, "id").String()
 			fmt.Println("Collector ID: " + collectorID)
@@ -143,11 +138,10 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	} else {
 		jsonBody = []byte(`{"type":"invite","subject":"` + subject + `","recipient_status":"` + recipientStatus + `"}`)
 	}
-	reqMessageID := callUrl(method, message_url, bytes.NewBuffer(jsonBody), accessToken)
-	containError = gjson.Get(string(reqMessageID), "Error").String()
-	if containError != "" {
-		context.SetOutput("status", containError)
-		return true, nil
+	reqMessageID, err := callUrl(method, message_url, bytes.NewBuffer(jsonBody), accessToken)
+	if err != nil {
+		log.Debugf("Error while fetching messageID: [%s]", err.Error())
+		return false, err
 	} else {
 		messageID = gjson.Get(reqMessageID, "id").String()
 		fmt.Println("Message ID: " + messageID)
@@ -158,7 +152,6 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 		method = "POST"
 		recipient_url := "https://api.surveymonkey.com/v3/collectors/" + collectorID + "/messages/" + messageID + "/recipients/bulk"
 		emails := strings.Split(recipientList, ",")
-
 		emailParent_Json := `{ "contacts": [`
 		count := 0
 		for i := 0; i < len(emails); i++ {
@@ -171,11 +164,10 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 		}
 		emailParent_Json = emailParent_Json + "]}"
 		jsonBody = []byte(emailParent_Json)
-		reqRecipientBulk := callUrl(method, recipient_url, bytes.NewBuffer(jsonBody), accessToken)
-		containError = gjson.Get(string(reqRecipientBulk), "Error").String()
-		if containError != "" {
-			context.SetOutput("status", containError)
-			return true, nil
+		reqRecipientBulk, err := callUrl(method, recipient_url, bytes.NewBuffer(jsonBody), accessToken)
+		if err != nil {
+			log.Debugf("Error while attatching a message body :[%s]", err.Error())
+			return false, err
 		} else {
 			succStatus_1 := gjson.Get(reqRecipientBulk, "succeeded.#").String()
 			succStatus_2 := gjson.Get(reqRecipientBulk, "existing.#").String()
@@ -191,14 +183,14 @@ func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
 	//add schedule email date , API Call #5
 	method = "POST"
 	send_url := "https://api.surveymonkey.com/v3/collectors/" + collectorID + "/messages/" + messageID + "/send"
-	curr_date := time.Now().Add(-328 * time.Minute).Format("2006-01-02T15:04:05+00:00") //SUTRACTING 5:28 hrs FROM CURRENT TIMESTAMP SO THAT EMAIL CAN BE SENT AFTRE 2 MIN AFTER THE REQUEST HITS
+	curr_date := time.Now().Add(-328 * time.Minute).Format("2006-01-02T15:04:05+00:00") //SUBTRACTING 5:28 hrs FROM CURRENT TIMESTAMP SO THAT EMAIL CAN BE SENT 2 MIN AFTER THE REQUEST HITS
 	jsonBody = []byte(`{ "scheduled_date": "` + curr_date + `"}`)
-	reqSendMail := callUrl(method, send_url, bytes.NewBuffer(jsonBody), accessToken)
-	containError = gjson.Get(string(reqSendMail), "Error").String()
-	if containError != "" {
-		context.SetOutput("status", containError)
-		return true, nil
+	_, err = callUrl(method, send_url, bytes.NewBuffer(jsonBody), accessToken)
+	if err != nil {
+		log.Debugf("Error while sending emails: [%s]", err.Error())
+		return false, err
 	} else {
+		context.SetOutput("status", "Email sent succesfully")
 		fmt.Println("Email sent succesfully")
 	}
 	return true, nil
